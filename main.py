4111028752d1699c4bb30cf998ab1e0ad62afa84
Alexandru Dobos <dobosalex77@yahoo.com>
2021-11-30 21:09:13 +0200

    Am facut initializarea jocului pentru ca userul sa poata introduce date de input de la tastatura (sa-si aleaga categoria), si am introdus trei categorii (mancare, sport, tari) Urmeaza sa incerc o implementare pentru joc.

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+numberOfChances = 5
+continueGame = True
+semaphore = False
+categories = ['MANCARE', 'SPORT', 'TARI']
 
 
-# Press the green button in the gutter to run the script.
+def initialize():
+    print("-----   Introdu categoria [ 0 -", len(categories), "]   -----")
+    while True:
+        try:
+            for i in range(0, len(categories)):
+                print("[", i, "]", categories[i])
+            userInput = int(input("Ce categorie alegi?: "))
+        except ValueError:
+            print("Introdu o valoare corecta din cele prezentate.")
+            continue
+        else:
+            if userInput >= 0 and userInput < len(categories):
+                return int(userInput)
+            else:
+                print("--- !!! Introdu o valoare corecta din cele prezentate !!! ---")
+
+
+def openFile(index):
+    name = "categories\\" + categories[index]
+    file = open(name, 'r')
+    lines = []
+    for readLine in file:
+        line = readLine.strip()
+        lines.append(line)
+    return lines
+
+
+def userContinueGame():
+    userInput = input("Doresti sa incepi un joc nou? DA / NU: ")
+    while True:
+        if not (
+                userInput == "DA" or userInput == "NU" or userInput == "da" or userInput == "nu" or userInput == "Da" or userInput == "Nu"):
+            print("--- !!! INTRODU O VALOARE CORECTA !!! ---")
+            userInput = input("Doresti sa incepi un joc nou? DA / NU: ")
+        else:
+            if userInput == "DA" or userInput == "da" or userInput == "Da":
+                return True
+            else:
+                return False
+
+
+def game():
+    global continueGame
+    while continueGame:
+        category = initialize()
+        print("Categoria aleasa:", categories[category])
+        openFile(category)
+        if not userContinueGame():
+            continueGame = False
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    game()
